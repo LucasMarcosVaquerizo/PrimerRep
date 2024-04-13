fdd7afe73751d3ba18dd02efc897de026787f406
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2024-04-13 21:00:25 +0200

    espacios

---
+++ Main.java
@@ -0,0 +1,14 @@
+import com.google.gson.Gson;
+import es.uah.matcom.mp.el3.a.ejemplo.Persona;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+
+        Persona p = new Persona("Antonio MOratilla", 97);
+
+        String json = gson.toJson(p);
+
+        System.out.println(json);
+    }
+}
---
+++ es.uah.matcom.mp.el3.a/ejemplo/Persona.java
@@ -0,0 +1,28 @@
+package es.uah.matcom.mp.el3.a.ejemplo;
+
+public class Persona {
+    private String nombre = "Jhon Doe";
+    private int edad = 18;
+
+    public Persona(String nombre,int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String newNombre) {
+        this.nombre = newNombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int newEdad) {
+        this.edad = newEdad;
+    }
+
+}
